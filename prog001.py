4ebdb3ac1f93ee7fcbab1be65b198db41979024b
MarcBouchonville <34140846+MarcBouchonville@users.noreply.github.com>
2019-12-29 15:47:28 +0100

    Delete prog001.py

--- prog001.py
+++
@@ -1,9 +0,0 @@
-age = int ( input ("age du user : "))
-if age >= 18 :
-    if age >= 65 :
-        print ("Senior")
-    else :
-        print ("Adulte")
-else :
-    print ("Junior")
-print ("this is the end")
